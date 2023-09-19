190ede1e90105e539efe3b85e0486ac0086c0e38
MattTompkins <51715889+MattTompkins@users.noreply.github.com>
2023-09-19 19:51:37 +0100

    Create DB config

--- config/db.php
+++ config/db.php
@@ -1,3 +1,11 @@
 <?php
 
-/** DATABASE CONFIG */
+/** 
+ *  Database congig file for a MySQL Database.
+ *  Replace the details below with the correct credentials
+ */
+
+define( 'DBHOST','localhost' );
+define( 'DBUSER','username' );
+define( 'DBPASSWORD','password' );
+define( 'DBNAME','database name' );
--- src/classes/DB.php
+++ src/classes/DB.php
@@ -1,10 +1,19 @@
 <?php
 
-class DB {
+/**
+ * DB class for interacting with the database. DB credentials found in config/db.php
+ */
+class DB extends mysqli {
     
-    public static function test() {
-        echo 'hi';
-    }
+    private static $instance = null;
+
+    private $host = DBHOST;
+    private $user = DBUSER;
+    private $password = DBPASSWORD;
+    private $dbName = DBNAME;
+
+    
+
 }
 
 
